3b2745617363c65200d89ddd1df4ac8b34f6fd22
Qin Jiajia <jiajia.qin@intel.com>
2019-05-08 12:55:06 +0800

    Refactor matmul

--- src/kernels/matmul.ts
+++ src/kernels/matmul.ts
@@ -15,63 +15,105 @@
  * =============================================================================
  */
 
+// The differences with webgpu backend:
+// 1. Add postfix 'u' for uint const.
+// 2. use 'float result = A[*]; return result;' instead of 'return A[*];' to
+// workaround an ANGLE bug.
+
+import {computeDispatch} from '../webgl2compute_util';
+
 import {WebGL2ComputeProgram} from './webgl2compute_program';
 
-export class MatMulProgram implements WebGL2ComputeProgram {
-  outputShape: number[];
-  userCode: string;
-  workGroupSize: [number, number, number] = [16, 16, 1];
-  dispatch: [number, number, number];
-  variableNames = ['A', 'B'];
-  // M is A outer, N is shared, K is B outer
-  uniforms = 'uint M, N, K, batch;';
+export const matMulHeader = `
+  float mm_readA(uint row, uint col);
+  float mm_readB(uint row, uint col);
+  void mm_write(uint row, uint col, float value);
+  void mm_matMul(uint dimAOuter, uint dimInner, uint dimBOuter);`;
 
-  constructor(outputShape: [number, number, number]) {
-    this.outputShape = outputShape;
-    this.dispatch = [
-      Math.ceil(outputShape[1] / this.workGroupSize[0]),
-      Math.ceil(outputShape[2] / this.workGroupSize[1]), 1
-    ];
+export function makeMatMulSource(): string {
+  return `
+    ${matMulHeader}
 
-    this.userCode = `
-      const uint TileSize = gl_WorkGroupSize.x;
-      shared float Asub[TileSize][TileSize];
-      shared float Bsub[TileSize][TileSize];
+    const uint MatTileSize = gl_WorkGroupSize.x;  // .x == .y
+    shared float mm_Asub[MatTileSize][MatTileSize];
+    shared float mm_Bsub[MatTileSize][MatTileSize];
 
-      void main() {
-        uint row = gl_LocalInvocationID.y; // Local row ID (max: TileSize)
-        uint col = gl_LocalInvocationID.x; // Local col ID (max: TileSize)
-        uint globalRow = TileSize*gl_WorkGroupID.y + row; // Row ID of C (0..M)
-        uint globalCol = TileSize*gl_WorkGroupID.x + col; // Col ID of C (0..N)
+    void mm_matMul(uint dimAOuter, uint dimInner, uint dimBOuter) {
+        uint localRow = gl_LocalInvocationID.y;  // 0..MatTileSize
+        uint localCol = gl_LocalInvocationID.x;  // 0..MatTileSize
+        uint globalRow = gl_GlobalInvocationID.y;  // AOuter
+        uint globalCol = gl_GlobalInvocationID.x;  // Inner
 
         float acc = 0.0;
 
-        // Add 1 to N to ceil.
-        uint numTiles = (N % TileSize) != 0u ? N/TileSize + 1u : N/TileSize;
+        uint numTiles = (dimInner - 1u) / MatTileSize + 1u;
 
-        for (uint t=0u; t<numTiles; t++) {
+        for (uint t = 0u; t < numTiles; t++) {
           // Load one tile of A and B into local memory
-          uint tiledRow = TileSize*t + row;
-          uint tiledCol = TileSize*t + col;
-          Asub[row][col] = A[globalRow*N + tiledCol];
-          Bsub[row][col] = B[tiledRow*K + globalCol];
+          uint tiledACol = MatTileSize * t + localCol;
+          uint tiledBRow = MatTileSize * t + localRow;
+          mm_Asub[localRow][localCol] = mm_readA(globalRow, tiledACol);
+          mm_Bsub[localRow][localCol] = mm_readB(tiledBRow, globalCol);
 
           // Synchronise to make sure the tile is loaded
-          // memoryBarrierShared();
           barrier();
-          uint sizeTS =
-              (t == (numTiles - 1u) && ((N % TileSize) != 0u)) ? N % TileSize :  TileSize;
-          for (uint k=0u; k<sizeTS; k++) {
-            acc += Asub[row][k] * Bsub[k][col];
+
+          for (uint k = 0u; k < MatTileSize; k++) {
+            acc += mm_Asub[localRow][k] * mm_Bsub[k][localCol];
           }
 
           // Synchronise before loading the next tile
           barrier();
         }
 
-        if(globalCol < K && globalRow < M) {
-          setOutput(globalRow*K + globalCol, acc);
+        if (globalCol < dimBOuter && globalRow < dimAOuter) {
+          mm_write(globalRow, globalCol, acc);
         }
+      }
+  `;
+}
+
+export class MatMulProgram implements WebGL2ComputeProgram {
+  outputShape: number[];
+  userCode: string;
+  dispatch: [number, number, number];
+  variableNames = ['A', 'B'];
+  uniforms = 'uint dimAOuter, dimInner, dimBOuter, batch;';
+  workGroupSize: [number, number, number] = [16, 16, 1];  // Must be square.
+
+  constructor(outputShape: [number, number, number]) {
+    this.outputShape = outputShape;
+    const dispatchLayout = {x: [1], y: [2], z: [0]};
+    this.dispatch =
+        computeDispatch(dispatchLayout, this.outputShape, this.workGroupSize);
+
+    this.userCode = `
+      ${makeMatMulSource()}
+
+      float mm_readA(uint row, uint col) {
+        if (row < dimAOuter && col < dimInner) {
+          float result = A[row * dimInner + col];
+          return result;
+        } else {
+          return 0.0;
+        }
+      }
+
+      float mm_readB(uint row, uint col) {
+        if (row < dimInner && col < dimBOuter) {
+          float result = B[row * dimBOuter + col];
+          return result;
+        } else {
+          return 0.0;
+        }
+      }
+
+      void mm_write(uint row, uint col, float value) {
+        setOutput(row * dimBOuter + col, value);
+      }
+
+      void main() {
+        mm_matMul(dimAOuter, dimInner, dimBOuter);
       }
     `;
   }
